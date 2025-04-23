a5007c2cf40fba5c5db5976b1ca48ce63b607ef9
Pushseo <pushseo.kim@gmail.com>
2025-04-23 14:25:11 +0900

    feat: add icon asset

---
+++ src/shared/assets/icons.jsx
@@ -0,0 +1,22 @@
+import alert from "./icons/alert.svg";
+import chat from "./icons/chat.svg";
+import mountain from "./icons/mountain.svg";
+import note from "./icons/note.svg";
+import right from "./icons/right.svg";
+import search from "./icons/search.svg";
+import user from "./icons/user.svg";
+import users from "./icons/users.svg";
+
+const Icons = {
+  // side menu
+  alert,
+  chat,
+  mountain,
+  note,
+  right,
+  search,
+  user,
+  users,
+};
+
+export default Icons;
