8fbf126201280e966e9655e7d278e7b4b96351d0
Pushseo <pushseo.kim@gmail.com>
2025-05-13 11:08:23 +0900

    feat: PWA 서비스워커 등록

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,19 @@ import { BrowserRouter } from "react-router-dom";
 import "./app/styles/index.css";
 import App from "./app/App.jsx";
 
+import { registerSW } from "virtual:pwa-register";
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("새 버전이 있습니다. 새로고침하시겠습니까?")) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log("앱이 오프라인 모드로 준비되었습니다.");
+  },
+});
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
